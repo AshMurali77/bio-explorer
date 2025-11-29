697b794b45ba978b5ff4f465a8d1b9786412f16c
Ash Murali <ashmurali@Ashs-Air.lan>
2025-11-29 16:07:25 -0500

    Added in Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { BeakerIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+export default function Navbar() {
+  return (
+    <nav
+      className="
+      sticky top-0 left-0 right-0 
+      z-50 
+      backdrop-blur-md 
+      bg-slate-900/60 
+      border-b border-slate-800
+      px-6 py-4
+      flex items-center justify-between
+    "
+    >
+      <Link href="/" className="flex">
+        <BeakerIcon className="size-6 text-sky-500" />
+        <div className="ml-2 text-xl font-semibold">
+          Bioinformatics Explorer
+        </div>
+      </Link>
+
+      <div className="flex justify-center gap-3 text-md absolute left-1/2 -translate-x-1/2">
+        <Link href="/about" className="hover:text-sky-400 transition-colors">
+          About
+        </Link>
+        <Link href="/details" className="hover:text-sky-400 transition-colors">
+          Workflows
+        </Link>
+        <Link href="/details" className="hover:text-sky-400 transition-colors">
+          Tech
+        </Link>
+      </div>
+      <a href="https://www.linkedin.com/in/ashmurali/" target="_blank" className="bg-sky-600 text-white font-sm px-4 py-2 rounded-md max-w-50 transition-all duration-200 hover:bg-sky-400  hover:-translate-y-0.5 active:translate-y-0 focus:outline-none focus:ring-2 focus:ring-sky-400/50 flex">
+        <p>Let's Connect</p>
+        <ArrowRightIcon className="my-auto ml-2 size-5"/>
+      </a>
+    </nav>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ export default function Home() {
   return (
     <main className="min-h-screen py-8">
       <div className=" mx-12 items-center justify-center font-sans">
-        <h1 className="text-3xl font-bold mx-auto my-3">
-          Bioinformatics Timeline
-        </h1>
+
         <div className="relative">
           <div className="absolute left-1/2 top-0 bottom-0 -translate-x-1/2 w-1 bg-slate-800"></div>
               {TIMELINE_ITEMS.map((item, index)=>{
